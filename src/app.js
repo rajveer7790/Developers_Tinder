28103bf320772bb71395dc7c1de0903ad37c67bc
rajveer7790 <rajchaudhary.nie@gmail.com>
2025-07-08 02:05:24 +0530

    ✅ Setup Express server with routes and error handling

--- src/app.js
+++ src/app.js
@@ -2,15 +2,38 @@
 // It listens on port 3000 and logs a message when the server starts    
 const express = require("express");
 const app = express();
-app.use((req,res) => {
-  res.send("Hello, World!");
+// app.use((req,res) => {
+//   res.send("Hello, World!");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({firstName: "John", lastName: "Doe"});
 });
+app.post("/user", (req, res) => {
+  res.send({message: "User created successfully!"});
+}); 
+
 app.use("/tasks", (req,res) => {
   res.send("Hello, Tasks !");
+
 });
-app.use("/tasks1", (req,res) => {
-  res.send("Hello, Tasks131131 !");
+app.get("/getUserData", (req, res) => {
+  try {
+    // Simulate an error
+    throw new Error("Simulated error");
+    res.send("user data saved successfully");  
+  } catch (error) {
+   res.send("Error occurred while saving user data");
+  }
+  
 });
+app.use("/",(req, res) => {
+  if(err){
+  res.status(404).send("404 Not Found");
+  }
+  
+});
+
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log("✅ Server running at http://localhost:3000");
 });
