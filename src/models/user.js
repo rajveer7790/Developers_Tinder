d3a085c887974b7400a2689e45a544b4a4c5b4a3
rajveer7790 <rajchaudhary.nie@gmail.com>
2025-07-11 21:58:52 +0530

    Schema validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,47 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Importing mongoose for MongoDB object modeling
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String,
-        required: true
+        required: true,
+        minlength: 3, // Minimum length for first name
+        maxlength: 50 // Maximum length for first name
     },
     lastName: {
         type: String,
-        required: true
+        //required: true
     },
     email: {
+        lowercase: true, // Ensures email is stored in lowercase
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true // Removes whitespace from both ends of the string
     },
     password: {
         type: String,
         required: true
-    }
-   
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female", "other"]
+    },
+    about: {
+        type: String,
+        default: "This is a user",
+    },
+    skills: {
+        type: [String],
+        
+    },
+    age: {
+        type: Number,
+        min: 18, // Minimum age
+        max: 120
+     }
+},
+{
+    timestamps: true ,// Automatically adds createdAt and updatedAt fields
 });
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema); // User model
 module.exports = User;
 // This code defines a Mongoose schema and model for a user in a MongoDB database.
